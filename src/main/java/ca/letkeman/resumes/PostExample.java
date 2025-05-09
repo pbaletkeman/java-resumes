23717bae9bef344226442e7dcaf2823884eb0fbd
Pete Letkeman <pete@letkeman.ca>
2025-05-08 20:14:00 -0400

    added static prompt

--- src/main/java/ca/letkeman/resumes/PostExample.java
+++ src/main/java/ca/letkeman/resumes/PostExample.java
@@ -1,6 +1,8 @@
 package ca.letkeman.resumes;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -8,7 +10,7 @@ import okhttp3.Request.Builder;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class PostExample {
+public final class PostExample {
   public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
   final OkHttpClient client = new OkHttpClient();
@@ -24,9 +26,57 @@ public class PostExample {
     }
   }
 
-  String bowlingJson() {
+  static String readFileAsString(String fileName)  {
+    String data = "";
+    try {
+      data = new String(
+          Files.readAllBytes(Paths.get(fileName)));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return data;
+  }
+
+
+  class ModelParameters {
+    private String model;
+    private float temperature;
+    private String url;
+
+    public String getModel() {
+      return model;
+    }
+
+    public void setModel(String model) {
+      this.model = model;
+    }
+
+    public float getTemperature() {
+      return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+      this.temperature = temperature;
+    }
+
+    public String getUrl() {
+      return url;
+    }
+
+    public void setUrl(String url) {
+      this.url = url;
+    }
+
+    public ModelParameters(String model, float temperature, String url) {
+      this.model = model;
+      this.temperature = temperature;
+      this.url = url;
+    }
+  }
+
+  String bowlingJson(String model, float temperature) {
     String y = "{\n"
-        + "    \"model\": \"gemma-3-4b-it\",\n"
+        + "    \"model\": \"" + model + "\",\n"
         + "    \"messages\": [\n"
         + "        {\n"
         + "            \"role\": \"system\",\n"
@@ -37,7 +87,7 @@ public class PostExample {
         + "            \"content\": \"What day is it today?\"\n"
         + "        }\n"
         + "    ],\n"
-        + "    \"temperature\": 0.7,\n"
+        + "    \"temperature\": " + temperature + ",\n"
         + "    \"max_tokens\": -1,\n"
         + "    \"stream\": false\n"
         + "}";
@@ -46,7 +96,7 @@ public class PostExample {
 
   public static void main(String[] args) throws IOException {
     PostExample example = new PostExample();
-    String json = example.bowlingJson();
+    String json = example.bowlingJson("gemma-3-4b-it",0.7f);
     String response = example.post(json);
     System.out.println(response);
   }
