98ca8529aede788c2f24310ef6ee70551f5458d9
Pete Letkeman <pete@letkeman.ca>
2025-05-14 18:35:02 -0400

    refactored markdown to pdf code

--- src/main/java/ca/letkeman/resumes/HtmlToPdf.java
+++ src/main/java/ca/letkeman/resumes/HtmlToPdf.java
@@ -10,25 +10,60 @@ import org.commonmark.renderer.html.HtmlRenderer;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.xhtmlrenderer.extend.FontResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 public class HtmlToPdf {
-  public static void main(String[] args) throws IOException {
 
-    String markdownContent = "";
-    try {
-      markdownContent = new String(Files.readAllBytes(Paths.get("sample/resume.md")));
-    } catch (IOException e) {
-      System.out.println(e);
+  private static Logger logger = LoggerFactory.getLogger(HtmlToPdf.class);
+
+  private String markdownFilePath;
+  private String pdfFilePath;
+  private String markdownContent;
+
+
+  public static Logger getLogger() {
+    return logger;
+  }
+
+  public static void setLogger(Logger logger) {
+    HtmlToPdf.logger = logger;
+  }
+
+  public String getMarkdownContent() {
+    return markdownContent;
+  }
+
+  public void setMarkdownContent(String markdownContent) {
+    this.markdownContent = markdownContent;
+  }
+
+  public String getMarkdownFilePath() {
+    return markdownFilePath;
+  }
+
+  public void setMarkdownFilePath(String markdownFilePath) {
+    this.markdownFilePath = markdownFilePath;
+  }
+
+  public String getPdfFilePath() {
+    return pdfFilePath;
+  }
+
+  public void setPdfFilePath(String pdfFilePath) {
+    this.pdfFilePath = pdfFilePath;
+  }
+
+  public HtmlToPdf(){}
+
+  public HtmlToPdf(String markdownFilePath, String pdfFilePath, String markdownContent) {
+    if (markdownContent.isBlank()) {
+      setMarkdownFilePath(markdownFilePath);
+    } else {
+      setMarkdownContent(markdownContent);
     }
-    Parser parser = Parser.builder().build();
-    Node document = parser.parse(markdownContent);
-    HtmlRenderer renderer = HtmlRenderer.builder().build();
-    String HTMLContent = renderer.render(document);
-
-    String xhtml = htmlToXhtml(HTMLContent);
-    xhtmlToPdf(xhtml, "output.pdf");
+    setPdfFilePath(pdfFilePath);
   }
 
   private static String htmlToXhtml(String html) {
@@ -40,11 +75,42 @@ public class HtmlToPdf {
   private static void xhtmlToPdf(String xhtml, String outFileName) throws IOException {
     File output = new File(outFileName);
     ITextRenderer iTextRenderer = new ITextRenderer();
-    FontResolver resolver = iTextRenderer.getFontResolver();
     iTextRenderer.setDocumentFromString(xhtml);
     iTextRenderer.layout();
     OutputStream os = new FileOutputStream(output);
     iTextRenderer.createPDF(os);
     os.close();
+  }
+
+  public boolean convertFile (){
+    String content = "";
+    if (getMarkdownContent() == null || getMarkdownContent().isBlank()) {
+      try {
+        content = new String(Files.readAllBytes(Paths.get(getMarkdownFilePath())));
+      } catch (IOException e) {
+        logger.error("Error unable to read markdown file:\n{}", e.toString());
+        return false;
+      }
+    } else {
+      content = getMarkdownContent();
+    }
+    Parser parser = Parser.builder().build();
+    Node document = parser.parse(content);
+    HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+    try {
+      xhtmlToPdf(htmlToXhtml(renderer.render(document)), getPdfFilePath());
+    } catch (IOException e) {
+      logger.error("Error unable to save PDF file:\n{}", e.toString());
+      return false;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+
+    HtmlToPdf htmlToPdf = new HtmlToPdf("sample/resume.md","output.pdf", "");
+    htmlToPdf.convertFile();
+
   }
 }
