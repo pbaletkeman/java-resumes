9fd002f0e0d4723b449e9f7a12defa88780f1087
Pete Letkeman <pete@letkeman.ca>
2025-05-30 22:20:45 -0400

    started secondary elements layout

---
+++ frontend/src/components/FileData.tsx
@@ -0,0 +1,17 @@
+export const Filedata = [
+  {
+    name: "Peter Letkeman-Resume.md",
+    url: "http://localhost:8080/files/Peter Letkeman-Resume.md",
+    size: "6 kb",
+  },
+  {
+    name: "Peter Letkeman-Resume.txt",
+    url: "http://localhost:8080/files/Peter Letkeman-Resume.txt",
+    size: "6 kb",
+  },
+  {
+    name: "PointClickCare-Software Engineer.txt",
+    url: "http://localhost:8080/files/PointClickCare-Software Engineer.txt",
+    size: "3 kb",
+  },
+];
---
+++ frontend/src/components/MD2PDF.tsx
@@ -0,0 +1,12 @@
+import "primeicons/primeicons.css";
+
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+
+export default function MD2PDF() {
+  return (
+    <Card className="border-round-3xl">
+      <Button>Convert</Button>
+    </Card>
+  );
+}
